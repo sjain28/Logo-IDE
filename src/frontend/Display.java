881cd5036a0116691e9675742324a05e01c2a360
bobbywang000 <bcw22@duke.edu>
2016-02-29 11:26:31 -0500

    Front End Skeleton

---
+++ src/frontend/Display.java
@@ -0,0 +1,23 @@
+package frontend;
+
+import javafx.scene.Scene;
+
+public class Display  extends Window{
+
+	public Display(double width, double height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Scene init() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void step(double elapsedTime) {
+		// TODO Auto-generated method stub
+	}
+	
+}
---
+++ src/global/Main.java
@@ -0,0 +1,49 @@
+package global;
+
+import frontend.*;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+/**
+ * This is the main program, it is basically boilerplate to create
+ * an animated scene.
+ * 
+ * @author Robert C. Duvall
+ */
+public class Main extends Application 	
+{
+    public static final int FRAMES_PER_SECOND = 60;
+    private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+    private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+
+    private static Display display;
+
+    @Override
+    public void start (Stage s) 
+    {
+    	//make sure to initialise backend too
+        display = new Display(50, 50);
+        s.setTitle(display.getTitle());
+
+        Scene mainScene = display.init();
+        s.setScene(mainScene);
+        s.show();
+        
+		KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY),
+		                e -> display.step(SECOND_DELAY));
+		                
+		Timeline animation = new Timeline();
+		animation.setCycleCount(Timeline.INDEFINITE);
+		animation.getKeyFrames().add(frame);
+		animation.play();
+        
+    }
+    
+    public static void main (String[] args) {
+        launch(args);
+    }
+}
